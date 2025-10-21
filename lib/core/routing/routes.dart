b9511e8086af79e780531bed0369b859d75400bd
Administrator <engmostafasoliman0@gmail.com>
2025-10-21 16:49:37 +0400

    onboard commit

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,6 @@
+class Routes {
+  static const String splash = '/splashScreen';
+  static const String onBoarding = '/onBoardingScreen';
+  static const String home = '/homeScreen';
+  static const String login = '/loginScreen';
+}
---
+++ lib/core/theming/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primaryColor = Color(0xFF247CFF);
+  static const Color secondaryColor = Color(0xFFFFC107);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
+  static const Color gray = Color(0xFF757575);
+  static const Color textColor = Color(0xFF000000);
+}
