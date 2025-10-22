d13dbb2d46a2a01738c81289e98c1692b314c6c3
Administrator <engmostafasoliman0@gmail.com>
2025-10-22 15:10:53 +0400

    creating splash screen for all mobile app -android -android 12 -ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:doctor/core/routing/app_router.dart';
 import 'package:doctor/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-
+  FlutterNativeSplash.preserve(widgetsBinding: WidgetsBinding.instance);
   runApp(DocApp(appRouter: AppRouter()));
 }
