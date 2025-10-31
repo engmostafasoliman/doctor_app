90c536574b25ca37b4c98f5bef842d90ec50cbcc
Administrator <engmostafasoliman0@gmail.com>
2025-10-31 15:58:56 +0400

    -add flavor (Development - Production ). for android -changed android app name for each flavors. -add launch.json for flavors configs . -create two main for prodution and development

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -2,11 +2,13 @@ import 'package:doctor/core/routing/app_router.dart';
 import 'package:doctor/doc_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import 'core/DI/dependency_injection.dart';
 
-void main() {
+void main() async {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   setupDependencyInjection();
   runApp(DocApp(appRouter: AppRouter()));
