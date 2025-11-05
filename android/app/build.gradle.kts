58d153947954c322e6241ff942cd4480dc900d3c
Administrator <engmostafasoliman0@gmail.com>
2025-11-05 15:10:01 +0400

    -add fastlane and connected it to firebase distribution -Bumped version 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("com.android.application")
+    // START: FlutterFire Configuration
+    id("com.google.gms.google-services")
+    // END: FlutterFire Configuration
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
@@ -39,7 +42,7 @@ android {
         }
         create("production") {
             dimension = "default"
-            applicationIdSuffix = ".production"
+            applicationIdSuffix = ""
             resValue("string", "app_name", "Al Doc Production")
             
         }
