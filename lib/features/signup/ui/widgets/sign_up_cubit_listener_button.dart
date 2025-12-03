f9bda16df9c7f9f74746fdc9e817f68fd2361daa
Administrator <engmostafasoliman0@gmail.com>
2025-12-03 14:40:53 +0400

    connect to github and window

--- lib/features/signup/ui/widgets/sign_up_cubit_listener_button.dart
+++ lib/features/signup/ui/widgets/sign_up_cubit_listener_button.dart
@@ -1,8 +1,6 @@
 import 'package:doctor/core/helpers/extentions.dart';
 import 'package:doctor/core/theming/style.dart';
 import 'package:doctor/core/widgets/app_text_button.dart';
-
-import 'package:doctor/features/login/logic/cubit/login_state.dart';
 import 'package:doctor/features/login/ui/login_screen.dart';
 import 'package:doctor/features/signup/logic/sign_up_cubit.dart';
 import 'package:flutter/material.dart';
