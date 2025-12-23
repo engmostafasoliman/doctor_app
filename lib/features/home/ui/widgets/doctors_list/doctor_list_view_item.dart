6efee9f593ea2485414976d6734d6d598fc1bba9
Administrator <engmostafasoliman0@gmail.com>
2025-12-23 15:59:03 +0400

    -add inhance shimmer  image

--- lib/features/home/ui/widgets/doctors_list/doctor_list_view_item.dart
+++ lib/features/home/ui/widgets/doctors_list/doctor_list_view_item.dart
@@ -1,9 +1,11 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:doctor/core/helpers/spacing.dart';
 import 'package:doctor/core/theming/colors.dart';
 import 'package:doctor/core/theming/style.dart';
 import 'package:doctor/features/home/data/models/specialization_response_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shimmer/shimmer.dart';
 
 class DoctorListViewItem extends StatelessWidget {
   const DoctorListViewItem({super.key, this.doctorsModel});
@@ -15,16 +17,33 @@ class DoctorListViewItem extends StatelessWidget {
       margin: EdgeInsets.only(bottom: 10.h),
       child: Row(
         children: [
-          Container(
-            height: 100.h,
-            width: 100.w,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8.r),
-              color: ColorManager.moreLighterGray,
-            ),
-            child: Image.asset(
-              'assets/images/doctor_image.png',
-              fit: BoxFit.contain,
+          CachedNetworkImage(
+            fit: BoxFit.contain,
+            imageUrl:
+                "https://media.istockphoto.com/id/465462879/photo/arab-saudi-doctor-man-posing-happy.jpg?s=612x612&w=0&k=20&c=0mEHK3v-Xi1xllnEEixcfzVgDere5sX_i0AJsG4MB-8=",
+            progressIndicatorBuilder: (context, url, downloadProgress) {
+              return Shimmer.fromColors(
+                baseColor: ColorManager.lightGray,
+                highlightColor: Colors.white,
+                child: Container(
+                  width: 110.w,
+                  height: 120.h,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(12.0),
+                    color: Colors.white,
+                  ),
+                ),
+              );
+            },
+            imageBuilder: (context, imageProvider) => Container(
+              width: 110.w,
+              height: 120.h,
+              decoration: BoxDecoration(
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12.0),
+                image: DecorationImage(image: imageProvider, fit: BoxFit.cover),
+              ),
             ),
           ),
           SizedBox(width: 16.w),
--- lib/features/home/ui/widgets/doctors_list/doctors_bloc_builder.dart
+++ lib/features/home/ui/widgets/doctors_list/doctors_bloc_builder.dart
@@ -1,6 +1,6 @@
 import 'package:doctor/features/home/logic/cubit/home_cubit.dart';
 import 'package:doctor/features/home/logic/cubit/home_state.dart';
-import 'package:doctor/features/home/ui/widgets/doctors_list_view.dart';
+import 'package:doctor/features/home/ui/widgets/doctors_list/doctors_list_view.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
